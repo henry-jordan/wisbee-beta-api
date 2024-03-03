50d4839665ab49a3a23a51e7891cc260e795a613
Henry Jordan <henryjordanemail@icloud.com>
2024-03-03 15:50:36 -0800

    Cors

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 import uvicorn
 
 app = FastAPI()
 handler = Mangum(app)
+
+origins = [
+    'http://localhost',
+    'http://localhost:5000'
+    'http://localhost:5000/websocketDebug'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 async def root():
